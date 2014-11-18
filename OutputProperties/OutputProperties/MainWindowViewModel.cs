124ad2d1c04154df6fadc13c12e8b9810a7f2165
Jimmy Byrd <jimmybyrd@gmail.com>
2014-11-17 21:53:28 -0500

    Change over to output properties

--- OutputProperties/OutputProperties/MainWindowViewModel.cs
+++ OutputProperties/OutputProperties/MainWindowViewModel.cs
@@ -1,70 +1,56 @@
-﻿using System.ComponentModel;
-using System.Runtime.CompilerServices;
-using OutputProperties.Annotations;
+﻿using System.Reactive.Linq;
+using ReactiveUI;
 
 namespace OutputProperties
 {
-    public class MainWindowViewModel : INotifyPropertyChanged
+    public class MainWindowViewModel : ReactiveObject
     {
         private string _firstName;
         private string _lastName;
         private string _favoriteColor;
-        public event PropertyChangedEventHandler PropertyChanged;
 
-        [NotifyPropertyChangedInvocator]
-        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+
+        ObservableAsPropertyHelper<string> _fullName;
+        public string FullName
         {
-            PropertyChangedEventHandler handler = PropertyChanged;
-            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+            get { return _fullName.Value; }
+        }
+
+
+        ObservableAsPropertyHelper<string> _sentence;
+        public string Sentence
+        {
+            get { return _sentence.Value; }
+        }
+        public MainWindowViewModel()
+        {
+            this.WhenAnyValue(x => x.FirstName, x => x.LastName, (first, last) => new {first,last})
+                .Select(name => string.Format("Full Name: {0} {1}", name.first, name.last))
+                .ToProperty(this, x => x.FullName, out _fullName);
+
+            this.WhenAnyValue(x => x.FullName, x => x.FavoriteColor, (full,color) => new {full,color})
+                .Select(x => string.Format("{0}'s favorite color is: {1}", x.full, x.color))
+                .ToProperty(this, x => x.Sentence, out _sentence);
         }
 
         public string FirstName
         {
             get { return _firstName; }
-            set
-            {
-                if (value == _firstName) return;
-                _firstName = value;
-                OnPropertyChanged();
-                OnPropertyChanged("FullName");
-                OnPropertyChanged("Sentence");
-            }
+            set { this.RaiseAndSetIfChanged(ref _firstName, value); }
         }
 
         public string LastName
         {
             get { return _lastName; }
-            set
-            {
-                if (value == _lastName) return;
-                _lastName = value;
-                OnPropertyChanged();
-                OnPropertyChanged("FullName");
-                OnPropertyChanged("Sentence");
-            }
-        }
-
-        public string FullName
-        {
-            get { return string.Format("Full Name: {0} {1}", FirstName, LastName); }
+            set { this.RaiseAndSetIfChanged(ref _lastName, value); }
         }
 
         public string FavoriteColor
         {
             get { return _favoriteColor; }
-            set
-            {
-                if (value == _favoriteColor) return;
-                _favoriteColor = value;
-                OnPropertyChanged();
-                OnPropertyChanged("Sentence");
-            }
+            set { this.RaiseAndSetIfChanged(ref _favoriteColor, value); }
         }
 
-        public string Sentence
-        {
-            get { return string.Format("{0}'s favorite color is: {1}", FullName, FavoriteColor); }
-        }
      
     }
 }
